b2293e3e0b6cd9064ba892334068a08944869908
ADARSHS993 <p12adarsh567@gmail.com>
2025-07-09 10:56:21 +0530

    ttggg

--- Program.cs
+++ Program.cs
@@ -29,32 +29,32 @@ namespace _23SOECE11067_ADARSH_PATEL_BD
 
             //Email: abc @ymail.com
 
-            Console.WriteLine("Name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("D.O.B:");
-            string dob = Console.ReadLine();
-            Console.WriteLine("Address :");
-            string add = Console.ReadLine();
-            Console.WriteLine("City:");
-            string city = Console.ReadLine();
-            Console.WriteLine("Pincode:");
-            int pin = int.Parse(Console.ReadLine());
-            Console.WriteLine("State :");
-            string State = Console.ReadLine();
-            Console.WriteLine("Country :");
-            string c1 = Console.ReadLine();
-            Console.WriteLine("Email :");
-            string email = Console.ReadLine();
-            Console.WriteLine("Name:" + name);
-            Console.WriteLine("D.O.B:" + dob);
-            Console.WriteLine("Address:" + add);
-            Console.WriteLine("City:" + city);
-            Console.WriteLine("Pincode:" + pin);
-            Console.WriteLine("State:" + State);
-            Console.WriteLine("Country:" + c1);
-            Console.WriteLine("Email:" + email);
-
-
+            //Console.WriteLine("Name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("D.O.B:");
+            //string dob = Console.ReadLine();
+            //Console.WriteLine("Address :");
+            //string add = Console.ReadLine();
+            //Console.WriteLine("City:");
+            //string city = Console.ReadLine();
+            //Console.WriteLine("Pincode:");
+            //int pin = int.Parse(Console.ReadLine());
+            //Console.WriteLine("State :");
+            //string State = Console.ReadLine();
+            //Console.WriteLine("Country :");
+            //string c1 = Console.ReadLine();
+            //Console.WriteLine("Email :");
+            //string email = Console.ReadLine();
+            //Console.WriteLine("Name:" + name);
+            //Console.WriteLine("D.O.B:" + dob);
+            //Console.WriteLine("Address:" + add);
+            //Console.WriteLine("City:" + city);
+            //Console.WriteLine("Pincode:" + pin);
+            //Console.WriteLine("State:" + State);
+            //Console.WriteLine("Country:" + c1);
+            //Console.WriteLine("Email:" + email);
+            ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+                h
 
 
 
